71b9274b797642c15591ee26679a87cfc798460a
Kashif Imam <kashifimam72@gmail.com>
2019-06-22 12:19:16 +0545

    Test For 3rd Party sdk

---
+++ app/src/main/java/com/test/testthirdsdk/MyApp.kt
@@ -0,0 +1,10 @@
+package com.test.testthirdsdk
+
+import com.iserveu.aeps.aepslibrary.utils.MatmApplication
+
+/**
+ * Created by kashifimam on 22,June,2019
+ */
+class MyApp : MatmApplication() {
+
+}
